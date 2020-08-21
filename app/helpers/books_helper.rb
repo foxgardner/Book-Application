f90d2ce5e6fe9e1d1e6ae3c55379c9cd71afaf08
Fox Gardner <fox@gardnermedia.com>
2020-08-21 15:05:08 -0600

    Added shelf & reworked book show logic.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -8,17 +8,17 @@ module BooksHelper
             book = Book.offset(i).first
             book_js <<  "
                         <script>
-                            var geometry = new THREE.BoxGeometry(#{book.width}, #{book.height}, 1);
+                            var geometry = new THREE.BoxGeometry(#{book.width}, #{book.height}, 3);
                             var material = new THREE.MeshLambertMaterial({color: #{book.color}})
                             
                             var mesh = new THREE.Mesh(geometry, material);
                             mesh.position.y = -2;
-                            mesh.position.x = #{i} * 1.01;
+                            mesh.position.x = #{i} * 1.01 -5;
                             scene.add(mesh);
                         
                         </script>
                         "
-      end
+        end
     
         book_js.html_safe
     end
